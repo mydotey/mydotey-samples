1c0a87f6891fe9cf8ddbaf87e236ccd42f294290
Qiang Zhao <koqizhao@outlook.com>
2025-07-04 00:06:15 +0800

    rust: refine insertion sort

--- rust/algorithm/src/sort/insertion_sort.rs
+++ rust/algorithm/src/sort/insertion_sort.rs
@@ -11,13 +11,37 @@ fn _insertion_sort<T: PartialOrd>(arr: &mut [T], start: usize, end: usize) {
 
     _insertion_sort(arr, start, end - 1);
 
-    for i in start..end - 1 {
-        if arr[i] <= arr[end - 1] {
+    let f: fn(&[T], usize, usize, usize) -> usize = if (end - start) % 2 == 0 {
+        search
+    } else {
+        binary_search
+    };
+
+    let i = f(arr, end - 1, start, end - 1);
+    arr.insert(end - 1, i);
+}
+
+fn search<T: PartialOrd>(arr: &[T], value_index: usize, start: usize, end: usize) -> usize {
+    for i in start..end {
+        if arr[i] <= arr[value_index] {
             continue;
         }
+        return i;
+    }
 
-        arr.insert(end - 1, i);
-        break;
+    return end;
+}
+
+fn binary_search<T: PartialOrd>(arr: &[T], value_index: usize, start: usize, end: usize) -> usize {
+    if end - start <= 1 {
+        return end;
+    }
+
+    let mid = (start + end) / 2;
+    if arr[mid] < arr[value_index] {
+        search(arr, value_index, mid + 1, end)
+    } else {
+        search(arr, value_index, start, mid)
     }
 }
 
