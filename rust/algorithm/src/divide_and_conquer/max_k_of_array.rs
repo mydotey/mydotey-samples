8b1e2cadf0c78ff29dcb4bb9db763fa9e9e185f8
Qiang Zhao <koqizhao@outlook.com>
2025-07-12 10:41:16 +0800

    fix bug

--- rust/algorithm/src/divide_and_conquer/max_k_of_array.rs
+++ rust/algorithm/src/divide_and_conquer/max_k_of_array.rs
@@ -5,8 +5,8 @@ pub fn kth_max_of_array(arr: &[i32], k: usize) -> Option<i32> {
 
     if k == 1 {
         let mut max = arr[0];
-        for i in 0 + 1..arr.len() {
-            if arr[i] > arr[0] {
+        for i in 1..arr.len() {
+            if arr[i] > max {
                 max = arr[i];
             }
         }
@@ -30,6 +30,14 @@ pub fn kth_max_of_array(arr: &[i32], k: usize) -> Option<i32> {
     };
 }
 
+struct Solution;
+
+impl Solution {
+    fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        kth_max_of_array(&nums[0..nums.len()], k.try_into().unwrap_or_default()).unwrap_or_default()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,5 +59,25 @@ mod tests {
         assert_eq!(kth_max_of_array(&arr, 3), Some(2));
         assert_eq!(kth_max_of_array(&arr, 4), Some(2));
         assert_eq!(kth_max_of_array(&arr, 5), Some(1));
+    }
+
+    #[test]
+    fn test_kth_max_of_array3() {
+        let arr = [-1, 2, 0];
+        assert_eq!(kth_max_of_array(&arr, 1), Some(2));
+        assert_eq!(kth_max_of_array(&arr, 2), Some(0));
+        assert_eq!(kth_max_of_array(&arr, 3), Some(-1));
+    }
+
+    #[test]
+    fn test_find_kth_largest() {
+        let arr = vec![1, 2, 3, 4, 5];
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 0), 0);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 1), 5);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 2), 4);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 3), 3);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 4), 2);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 5), 1);
+        assert_eq!(Solution::find_kth_largest(arr.clone(), 6), 0);
     }
 }
