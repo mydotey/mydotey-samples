36e804b11cf24186e0e659c5d6f4c3d270ee2ea9
Qiang Zhao <koqizhao@outlook.com>
2025-07-14 00:06:12 +0800

    refine

--- rust/algorithm/src/divide_and_conquer/beautiful_array.rs
+++ rust/algorithm/src/divide_and_conquer/beautiful_array.rs
@@ -1,28 +1,27 @@
 use std::vec;
 
-pub fn beautiful_array(n: usize) -> Vec<usize> {
+pub fn beautiful_array(n: i32) -> Vec<i32> {
     return match n {
         0 => vec![],
         1 => vec![1],
         2 => vec![1, 2],
         _ => {
-            let mut result = beautiful_array(n - 1);
+            let result = beautiful_array(n - 1);
             'f1: for i in 0..n - 1 {
                 let mut result2 = result.clone();
-                result2.insert(i, n);
-                for j in 1..n - 1 {
+                result2.insert(i as usize, n);
+                for j in 1..(n as usize) - 1 {
                     for k in 0..j {
-                        for l in j + 1..n {
+                        for l in j + 1..(n as usize) {
                             if result2[k] + result2[l] == result2[j] * 2 {
                                 continue 'f1;
                             }
                         }
                     }
                 }
-                result = result2;
-                break;
+                return result2;
             }
-            result
+            panic!("No beautiful array found for n = {}", n);
         }
     };
 }
