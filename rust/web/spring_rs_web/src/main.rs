e35e3b562756afb8e2106c848006ac5646553f89
Qiang Zhao <koqizhao@outlook.com>
2025-07-15 00:08:26 +0800

    rust: impl spring-rs

--- rust/web/spring_rs_web/src/main.rs
+++ rust/web/spring_rs_web/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use spring::{auto_config, App};
+use spring_web::{get, route};
+use spring_web::{
+    extractor::Path, axum::response::IntoResponse,
+    WebConfigurator, WebPlugin,
+};
+
+#[auto_config(WebConfigurator)]
+#[tokio::main]
+async fn main() {
+    App::new()
+        .add_plugin(WebPlugin)
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello_world() -> impl IntoResponse {
+    "hello world"
+}
+
+#[route("/hello/{name}", method = "GET", method = "POST")]
+async fn hello(Path(name): Path<String>) -> impl IntoResponse {
+    format!("hello {name}")
 }
