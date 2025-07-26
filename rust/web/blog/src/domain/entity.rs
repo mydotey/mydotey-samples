85ab05d67fed881b3a85225fd9781c9b81c211ba
Qiang Zhao <koqizhao@outlook.com>
2025-07-26 13:41:04 +0800

    refine

--- rust/web/blog/src/domain/entity.rs
+++ rust/web/blog/src/domain/entity.rs
@@ -1,14 +1,13 @@
 use actix_web::test;
 use serde::{Deserialize, Serialize};
-use w_macro::entity_fields;
+use w_macro::{entity_field, entity_fields};
 
 #[entity_fields]
+#[entity_field(name = "title", ty = "String")]
+#[entity_field(name = "body", ty = "String")]
+#[entity_field(name = "published", ty = "bool")]
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct Article {
-    pub title: String,
-    pub body: String,
-    pub published: bool,
-}
+pub struct Article {}
 
 #[cfg(test)]
 mod tests {
@@ -25,8 +24,8 @@ mod tests {
         assert!(article.create_time.is_none());
         assert!(article.update_time.is_none());
         assert!(article.deleted.is_none());
-        assert!(article.title.is_empty());
-        assert!(article.body.is_empty());
-        assert!(!article.published);
+        assert!(article.title.is_none());
+        assert!(article.body.is_none());
+        assert!(!article.published.is_none());
     }
 }
--- rust/web/w-macro/src/lib.rs
+++ rust/web/w-macro/src/lib.rs
@@ -1,7 +1,41 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::parse::Parser;
 use syn::{ItemStruct, parse, parse_macro_input};
+
+use darling::FromMeta;
+
+#[derive(Debug, FromMeta, Default)]
+#[darling(derive_syn_parse)]
+struct EntityFieldArgs {
+    name: String,
+    ty: String,
+}
+
+#[proc_macro_attribute]
+pub fn entity_field(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut item_struct = parse_macro_input!(input as ItemStruct);
+    let EntityFieldArgs { name, ty } = match syn::parse(args) {
+        Ok(v) => v,
+        Err(e) => {
+            return e.to_compile_error().into();
+        }
+    };
+    let name = format_ident!("{}", name);
+    let ty = format_ident!("{}", ty);
+    if let syn::Fields::Named(ref mut fields) = item_struct.fields {
+        fields.named.push(
+            syn::Field::parse_named
+                .parse2(quote! { pub #name: Option<#ty> })
+                .unwrap(),
+        );
+    }
+
+    return quote! {
+        #item_struct
+    }
+    .into();
+}
 
 #[proc_macro_attribute]
 pub fn entity_fields(args: TokenStream, input: TokenStream) -> TokenStream {
