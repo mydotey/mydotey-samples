e14f4407974f6ab3fa68caa7b88c5db129525c3c
Qiang Zhao <koqizhao@outlook.com>
2025-08-14 23:23:17 +0800

    rust: add blog-spring project

---
+++ rust/web/blog-spring/src/main.rs
@@ -0,0 +1,19 @@
+use spring::{App, auto_config};
+use spring_web::{WebConfigurator, WebPlugin, axum::response::IntoResponse, extractor::Path};
+use spring_web::{get, route};
+
+#[auto_config(WebConfigurator)]
+#[tokio::main]
+async fn main() {
+    App::new().add_plugin(WebPlugin).run().await
+}
+
+#[get("/")]
+async fn hello_world() -> impl IntoResponse {
+    "hello world"
+}
+
+#[route("/hello/{name}", method = "GET", method = "POST")]
+async fn hello(Path(name): Path<String>) -> impl IntoResponse {
+    format!("hello {name}")
+}
